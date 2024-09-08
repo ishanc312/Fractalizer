0a784f05d0e5ddfb66282f256fb275ff93e177d9
ishanc312 <ishanc312@g.ucla.edu>
2024-09-07 22:35:19 -0400

    More renders

--- main.cpp
+++ main.cpp
@@ -8,11 +8,10 @@ using namespace glm;
 
 int main() {
     std::vector<std::shared_ptr<Hittable>> scene = {
-        std::make_shared<fractSphere>(vec3(0.5,0.5,0.5), 0.25),
-        std::make_shared<Sphere>(vec3(0,0,2), 1)
+        std::make_shared<Sphere>(vec3(0,0,50), 2)
     };
-    Palette testPalette(vec3(0.5f, 0.5f, 0.5f), vec3(0.5f, 0.5f, 0.5f), vec3(1.0f, 0.7f, 0.4f), vec3(0.0f, 0.15f, 0.20f));
-    Camera cam(vec3(0, 0, -4), 1.0, testPalette, 0.03, false);
+    Palette testPalette(vec3(0.8f, 0.5f, 0.4f), vec3(0.2f, 0.4f, 0.2f), vec3(2.0f, 1.0f, 1.0f), vec3(0.0f, 0.25f, 0.25f));
+    Camera cam(vec3(0, 0, 0), 2.0, testPalette, 0.01, false);
     cam.renderImage(scene);
     cam.outputImage();
     return 0;
